Fixed the Hamiltonian to be the negative log-likelihood. Finished the indep_normal test case.

test/indep_normal/indep_normal.py
@@ -20,20 +20,21 @@ test = tests.test
 test.argtypes = [ctypes.c_int,ctypes.c_int,ctypes.c_int,ctypes.c_double]
 test.restype = SampleResults
 
-num_params = 2
-num_samps = int(2e5)
+num_params = 10
+num_samps = int(1e4)
 results = test(num_params,num_samps,10,1/128.0)
 print(results.chain.accept_rate)
 
 x_true = np.array([results.x_true[i] for i in range(num_params)])
 chain = np.array([[results.chain.samples[i][j] for j in range(num_params)]
          for i in range(num_samps)])
-chain = chain[int(1e5):,:]
 likelihoods = np.array([results.chain.log_likelihoods[i] for i
                         in range(num_samps)])
 
+# Should see a nice converged chain.
 plt.plot(range(len(likelihoods)),likelihoods)
 plt.show()
 
+# Should see independent random normal variables.
 corner(chain,truths=x_true)
 plt.show()