9fb087184abfa91801e8bc3ac27a396a7326e7a5
pierfied <pierfied@email.arizona.edu>
2017-09-18 16:04:05 -0700

    Adding recover_params test.

---
+++ test/recover_params/recover_params.py
@@ -0,0 +1,43 @@
+import ctypes
+import numpy as np
+import matplotlib.pyplot as plt
+from corner import corner
+
+class SampleChain(ctypes.Structure):
+    _fields_ = [('num_samples', ctypes.c_int),
+                ('num_params', ctypes.c_int),
+                ('accept_rate', ctypes.c_double),
+                ('samples', ctypes.POINTER(ctypes.POINTER(ctypes.c_double))),
+                ('log_likelihoods', ctypes.POINTER(ctypes.c_double))]
+
+class RPSampleResults(ctypes.Structure):
+    _fields_ = [('m_true', ctypes.c_double),
+                ('b_true', ctypes.c_double),
+                ('chain', SampleChain)]
+
+tests = ctypes.cdll.LoadLibrary('../libtests.so')
+
+test = tests.RP_test
+test.argtypes = [ctypes.c_int,ctypes.c_int,ctypes.c_int,ctypes.c_double]
+test.restype = RPSampleResults
+
+num_data = 10
+num_samps = int(1e5)
+results = test(num_data,num_samps,1,1.0e-2)
+print(results.chain.accept_rate)
+
+# x_true = np.array([results.x_true[i] for i in range(num_data)])
+x_true = np.array([results.m_true,results.b_true])
+chain = np.array([[results.chain.samples[i][j] for j in range(2)]
+         for i in range(num_samps)])
+chain = chain[int(1e4):,:]
+likelihoods = np.array([results.chain.log_likelihoods[i] for i
+                        in range(num_samps)])
+
+# Should see a nice converged chain.
+plt.plot(range(len(likelihoods)),likelihoods)
+plt.show()
+
+# Should see independent random normal variables.
+corner(chain,truths=x_true)
+plt.show()
